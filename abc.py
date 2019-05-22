2e6e182fd46a97cb9447b7f94fc4390f488301ca
amitbhalla2019 <amitbhalla2019@outlook.com>
2019-05-23 00:36:12 +0530

    checking

--- abc.py
+++ abc.py
@@ -1,3 +1,4 @@
 print ("Learning this new exercise to create githooks")
 print ("My new entry in this date")
-print ("New Sample New One A")
+print ("New Sample New One AB")
+print ("Hi, once again")
